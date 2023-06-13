0dccea1a9c9d6c5315880514147fcd20a8c3f260
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2023-06-14 02:09:00 +0530

    Fetched data from API and rendered

--- src/Albums.jsx
+++ src/Albums.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-function Albums() {
-    return (
-        <section ClassName="album-container">
-            <div className="album">
-                <div className="album-details">
-                    <div className="album-desc">
-                        <h3>Album 1</h3>
-                        <h5>Title 1</h5>
-                    </div>
-                    <div className="action-btns">
-                        <button className="del-btn">
-                            <img src="./Assets/bin.png" className="del-icon" />
-                        </button>
-                        <button className="edit-btn">
-                            <img src="./Assets/setting.png" className="edit-icon"/>
-                            <p>Edit</p>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className="album">
-                <div className="add-album">
-                    <button>
-                        <img src="./Assets/plus.png" className="add-album-icon"/>
-                    </button>
-                        <h3>Add Album</h3>
-                    {/* <div className="album-desc">
+function Albums(props) {
+  const albumArr = props.albums;
+  console.log(albumArr);
+  return (
+    <section className="album-container">
+      <div className="album">
+        <div className="add-album">
+          <button>
+            <img src="./Assets/plus.png" className="add-album-icon" />
+          </button>
+          <h3>Add Album</h3>
+          {/* <div className="album-desc">
                     </div> */}
-                </div>
+        </div>
+      </div>
+      {albumArr.map((album, index) => (
+        <div
+          className="album"
+          key={album.id}
+          style={{
+            backgroundImage: `url(${album.image})`,
+            backgroundSize: 'cover',
+          }}
+        >
+          <div className="album-details">
+            <div className="album-desc">
+              <h3>Album {index + 1}</h3>
+              <h5>{album.title}</h5>
             </div>
-        </section>
-    )
+            <div className="action-btns">
+              <button className="del-btn">
+                <img src="./Assets/bin.png" className="del-icon" />
+              </button>
+              <button className="edit-btn">
+                <img src="./Assets/setting.png" className="edit-icon" />
+                <p>Edit</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
 
-export default Albums
+export default Albums;
--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,42 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import Albums from './Albums'
+import Albums from './Albums';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [albums, setAlbums] = useState([]);
 
-  // Getting album info from API
-  useEffect(function () {
-    fetch('https://jsonplaceholder.typicode.com/albums')
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data)
-        for (let i = 0; i < 10; i++) {
-          // console.log(data[i]);
-        }
-      });
-  });
-
-  //Getting photos
-  useEffect(function () {
-    fetch(
-      'https://api.unsplash.com/photos/?client_id=AVrJWWr8Yz41zGZmDzuaAjgAJQFbhHdCTYv7InDXNX8'
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-      })
-      .catch((error) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const albumsResponse = await fetch('https://jsonplaceholder.typicode.com/albums')
+        const albumsData = await albumsResponse.json();
+        const trimmedAlbumsData = albumsData.slice(0, 7).map((album) => ({
+          id: album.id,
+          title: album.title,
+          image: '',
+        }));
+       
+        const imageDataResponse = await fetch('https://api.unsplash.com/photos/?client_id=AVrJWWr8Yz41zGZmDzuaAjgAJQFbhHdCTYv7InDXNX8')
+        const imageData = await imageDataResponse.json();
+        const updatedAlbumsData = trimmedAlbumsData.map((album, index) => ({
+          ...album,
+          image: imageData[index].urls.small
+        }));
+        setAlbums(updatedAlbumsData);
+      } catch (error) {
         console.error('Error fetching data: ', error);
-      });
+      }
+    };
+    fetchData();
   }, []);
 
   return (
-    <>
+    <div className="container">
       <Navbar />
-      <Albums />
+      <Albums albums={albums} />
       <Footer />
-    </>
+    </div>
   );
 }
 
