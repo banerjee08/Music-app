2d03f997a14d4c114c117276efda0b0ace9e01e7
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2023-06-15 09:36:03 +0530

    edit feature added with bugs

--- src/Albums.jsx
+++ src/Albums.jsx
@@ -3,9 +3,48 @@ import React, { useState } from 'react';
 function Albums(props) {
   const albumArr = props.albums;
   const [title, setTitle] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editedTitle, setEditedTitle] = useState('');
+  const [currentAlbumId, setCurrentAlbumId] = useState(null);
+
+  const handleOpenModal = (id, title) => {
+    setCurrentAlbumId(id);
+    setEditedTitle(title);
+    setIsModalOpen(true);
+    // setTitle(title);
+    if (id !== null) {
+      setTitle('');
+    }
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setCurrentAlbumId(null);
+    setEditedTitle('');
+  };
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
+  };
+
+  const handleModalTitleChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
+
+  const handleUpdateTitle = () => {
+    if (editedTitle.trim() !== '') {
+      const updatedAlbum = {
+        id: currentAlbumId,
+        title: editedTitle.trim(),
+        image: '',
+      };
+      props.updateAlbum(updatedAlbum);
+      handleCloseModal();
+    }
+  };
+
+  const handleDeleteAlbum = (id) => {
+    props.deleteAlbum(id);
   };
 
   const handleSubmit = (e) => {
@@ -20,7 +59,7 @@ function Albums(props) {
     }
   };
 
-  console.log(albumArr);
+  // console.log(albumArr);
   return (
     <section className="album-container">
       <div className="album">
@@ -29,6 +68,7 @@ function Albums(props) {
             <button type="submit">
               <img src="./Assets/plus.png" className="add-album-icon" />
             </button>
+
             <input
               type="text"
               value={title}
@@ -56,10 +96,16 @@ function Albums(props) {
               <h5>{album.title}</h5>
             </div>
             <div className="action-btns">
-              <button className="del-btn">
+              <button
+                className="del-btn"
+                onClick={() => handleDeleteAlbum(album.id)}
+              >
                 <img src="./Assets/bin.png" className="del-icon" />
               </button>
-              <button className="edit-btn">
+              <button
+                className="edit-btn"
+                onClick={() => handleOpenModal(album.id, album.title)}
+              >
                 <img src="./Assets/setting.png" className="edit-icon" />
                 <p>Edit</p>
               </button>
@@ -67,6 +113,25 @@ function Albums(props) {
           </div>
         </div>
       ))}
+
+      {/* modal */}
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Edit Title</h3>
+            <input
+              type="text"
+              value={editedTitle}
+              onChange={handleModalTitleChange}
+              placeholder="Enter new Title"
+            />
+            <div className="modal-actions">
+              <button onClick={handleUpdateTitle}>Update</button>
+              <button onClick={handleCloseModal}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
